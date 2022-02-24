fe19b0ffb5de18cd1f1b19eb10f3ddc6e024a0b2
simone andreozzi <simoneandreozzi8@gmail.com>
2022-02-24 21:00:29 +0100

    esercizio-complex

--- js/main.js
+++ js/main.js
@@ -1,33 +1,38 @@
-/* const ul = document.querySelector ("ul.list"); */
+const listaNumeri = document.getElementById ("numeri"); 
+/* console.log("INIZIO") */
+
+for (let i = 1; i <= 100; i++) {  
 
 
-for (let i = 0; i <= 100; i++) {  
-
+    let contenuto = i;//buzzfiz,buzz,fizz oppure il numero;
+    
+    /* se sono numeri multipli di 3 e di 5 allora scrivimi in console buzzfizz */
     if ((i % 3 ==0 ) && (i % 5 ==0 )){
         console.log("buzzfizz");
-    }
-
-
-    else if (i % 3 ==0 )  {
-        console.log("fizz");
-        
+        contenuto = "buzzfizz";
     } 
+
+    /* se sono numeri mulyipli di 3 allora scrivimi fizz */
+     else if (i % 3 ==0 )  {
+        console.log("fizz");
+        contenuto = "fizz";
         
+    }  
+        /* se sono numeri multipli di 5 allora scrivimi buzz */
      else if (i % 5 ==0 ) {
         console.log("buzz");
+        contenuto = "buzz";
         
     } else {
         console.log(i); 
-    }
+    } 
     
+    let newbox = document.createElement("div")
+    newbox.classList.add("box");
+    newbox.innerHTML = contenuto;
+    listaNumeri.append(newbox);
+
     
-     
     
 
-   /*  if(i % 5 == 0){
-        console.log("buzz");
-    } 
-     else{
-         console.log(i);
-     } */
 }
